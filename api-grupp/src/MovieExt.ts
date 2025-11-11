8db5e31f703b3976da73e6f5cbbe5055017c642c
rorotarver <roro9722@outlook.com>
2025-11-11 14:14:59 +0100

    created variable x

--- api-grupp/src/MovieExt.ts
+++ api-grupp/src/MovieExt.ts
@@ -1,8 +1,11 @@
 import axios from "axios"
 
+
 const get = async () =>  {
     const response = await axios.get<MovieExt>("https://www.omdbapi.com/?i=tt3896198&apikey=f5a04bf9");
     return response.data;
 }
+
+const x = document.getElementById('btnMovie');
 
 get ();
