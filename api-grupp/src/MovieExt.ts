129bc4e76802422b8e38d992dbee555ed30fbe0a
rorotarver <roro9722@outlook.com>
2025-11-11 14:07:03 +0100

    added file movieext

---
+++ api-grupp/src/MovieExt.ts
@@ -0,0 +1,8 @@
+import axios from "axios"
+
+const get = async () =>  {
+    const response = await axios.get<MovieExt>("https://www.omdbapi.com/?i=tt3896198&apikey=f5a04bf9");
+    return response.data;
+}
+
+get ();
